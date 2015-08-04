340feac14a7f6ec4fe361bc9cf82443a7542c5b0
Chris Gregg <tofergregg@gmail.com>
2015-08-04 08:06:37 -0400

    Initial Commit

---
+++ SerialBeanTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SerialBeanTest
+//
+//  Created by Chris Gregg on 8/4/15.
+//  Copyright (c) 2015 Chris Gregg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SerialBeanTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SerialBeanTest
+//
+//  Created by Chris Gregg on 8/4/15.
+//  Copyright (c) 2015 Chris Gregg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
